3e0d5a3eb4836c19b4cad6a2485e3ace12a5c04e
H3AR7B3A7 <steven.d.hondt.sdh@gmail.com>
2020-08-29 11:05:04 +0200

    Data model and request

--- src/main/java/be/dog/d/steven/corona_tracker/CoronaTrackerApplication.java
+++ src/main/java/be/dog/d/steven/corona_tracker/CoronaTrackerApplication.java
@@ -2,8 +2,10 @@ package be.dog.d.steven.corona_tracker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class CoronaTrackerApplication {
 
     public static void main(String[] args) {
--- src/main/java/be/dog/d/steven/corona_tracker/services/CoronaTrackerDataService.java
+++ src/main/java/be/dog/d/steven/corona_tracker/services/CoronaTrackerDataService.java
@@ -1,21 +1,32 @@
 package be.dog.d.steven.corona_tracker.services;
 
+import be.dog.d.steven.corona_tracker.model.LocationStats;
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVRecord;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
 import java.io.IOException;
+import java.io.StringReader;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class CoronaTrackerDataService {
 
-    private static String CONFIRMED_DATA_URL = "https://github.com/CSSEGISandData/COVID-19/blob/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv";
+    private static String CONFIRMED_DATA_URL = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv";
+
+    private List<LocationStats> allDataList = new ArrayList<>();
 
     @PostConstruct
+    @Scheduled(cron = "* * 1 * * *")
     public void fetchData() throws IOException, InterruptedException {
+        List<LocationStats> newDataList = new ArrayList<>();
 
         HttpClient client = HttpClient.newHttpClient();
         HttpRequest request =HttpRequest.newBuilder()
@@ -23,6 +34,17 @@ public class CoronaTrackerDataService {
                 .build();
         HttpResponse<String> httpResponse = client.send(request, HttpResponse.BodyHandlers.ofString());
 
-        System.out.println(httpResponse.body());
+        StringReader csvBodyReader = new StringReader(httpResponse.body());
+
+        Iterable<CSVRecord> records = CSVFormat.DEFAULT.withFirstRecordAsHeader().parse(csvBodyReader);
+        for (CSVRecord record : records) {
+            LocationStats locationStats = new LocationStats();
+            locationStats.setState(record.get("Province/State"));
+            locationStats.setRegion(record.get("Country/Region"));
+            locationStats.setLatestTotalCases(Integer.parseInt(record.get(record.size()-1)));
+            System.out.println(locationStats);
+            newDataList.add(locationStats);
+        }
+        this.allDataList = newDataList;
     }
 }
