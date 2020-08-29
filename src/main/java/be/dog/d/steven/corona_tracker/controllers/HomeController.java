b1c69f2128b321b2aa3b50be63bc9750fb176ef4
H3AR7B3A7 <steven.d.hondt.sdh@gmail.com>
2020-08-29 17:14:42 +0200

    Calculating total in controller

--- src/main/java/be/dog/d/steven/corona_tracker/controllers/HomeController.java
+++ src/main/java/be/dog/d/steven/corona_tracker/controllers/HomeController.java
@@ -1,10 +1,14 @@
 package be.dog.d.steven.corona_tracker.controllers;
 
+import be.dog.d.steven.corona_tracker.model.LocationConfirmedStats;
+import be.dog.d.steven.corona_tracker.model.LocationDeathStats;
 import be.dog.d.steven.corona_tracker.services.CoronaTrackerDataService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @Controller
 public class HomeController {
@@ -14,8 +18,19 @@ public class HomeController {
 
     @GetMapping("/")
     public String home(Model model){
-        model.addAttribute("locationConfirmedStats",coronaTrackerDataService.getAllConfirmedDataList());
-        model.addAttribute("locationDeathStats",coronaTrackerDataService.getAllDeathDataList());
+        List<LocationConfirmedStats> allConfirmedStats = coronaTrackerDataService.getAllConfirmedDataList();
+        int totalConfirmed = allConfirmedStats.stream().mapToInt(confirmedStat -> confirmedStat.getLatestTotalCases()).sum();
+
+        model.addAttribute("locationConfirmedStats",allConfirmedStats);
+        model.addAttribute("totalConfirmed", totalConfirmed);
+
+        List<LocationDeathStats> allDeathStats = coronaTrackerDataService.getAllDeathDataList();
+        int totalDeath = allDeathStats.stream().mapToInt(deathStat -> deathStat.getLatestTotalDeath()).sum();
+
+        model.addAttribute("locationDeathStats",allDeathStats);
+        model.addAttribute("totalDeath", totalDeath);
+
+
         return "home";
     }
 }
