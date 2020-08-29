07072e466a8f783ba02795660de24907500edfbb
H3AR7B3A7 <steven.d.hondt.sdh@gmail.com>
2020-08-29 11:05:08 +0200

    Data model and request

---
+++ src/main/java/be/dog/d/steven/corona_tracker/model/LocationStats.java
@@ -0,0 +1,41 @@
+package be.dog.d.steven.corona_tracker.model;
+
+public class LocationStats {
+
+    private String state;
+    private String region;
+    private int latestTotalCases;
+
+    public String getState() {
+        return state;
+    }
+
+    public String getRegion() {
+        return region;
+    }
+
+    public int getLatestTotalCases() {
+        return latestTotalCases;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public void setRegion(String region) {
+        this.region = region;
+    }
+
+    public void setLatestTotalCases(int latestTotalCases) {
+        this.latestTotalCases = latestTotalCases;
+    }
+
+    @Override
+    public String toString() {
+        return "LocationStats{" +
+                "state='" + state + '\'' +
+                ", region='" + region + '\'' +
+                ", latestTotalCases=" + latestTotalCases +
+                '}';
+    }
+}
